b3b9f97cc7fa08f2318082873e55fdf8da011987
Ian Hamblin <ian@3deo.com>
2022-01-10 20:01:04 +0000

    Modified docker run script.

--- RunDockerServer.sh
+++ RunDockerServer.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker run  -p 80:80 -v $SCRIPT_DIR:/usr/local/apache2/htdocs/ --name aquaweb-1  httpd
+docker run  -p 80:80 -v $SCRIPT_DIR/dist:/usr/local/apache2/htdocs/ --name aquaweb-1  httpd
